7636a231bfa1fa1a3d14cab7869964a99cc81a79
Andrea Ostuni <andrea19934@gmail.com>
2022-11-17 13:55:06 +0000

    fourth try

--- .github/actions/lint/run.sh
+++ .github/actions/lint/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+echo "$SSH_AUTH_SOCK"
 ssh -T git@github.com
 ./setup.sh
 ament_${LINTER} src/
--- .github/actions/test/run.sh
+++ .github/actions/test/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-git config --global user.email "andrea19934@google.com"
-git config --global user.name "Andrea Ostuni"
+# git config --global user.email "andrea19934@google.com"
+# git config --global user.name "Andrea Ostuni"
+echo "$SSH_AUTH_SOCK"
 ssh -T git@github.com
 ./setup.sh
 ./build.sh

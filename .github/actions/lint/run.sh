e953fbae58491e00cd84b0e86f7c6ff5473e1dc5
Andrea Ostuni <andrea19934@gmail.com>
2022-11-17 14:09:22 +0000

    6 try

--- .github/actions/lint/run.sh
+++ .github/actions/lint/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 echo "$SSH_AUTH_SOCK"
+eval "$(ssh-agent)"
 ssh-add -L
 cat /etc/ssh_config
 ssh -T git@github.com
--- .github/actions/test/run.sh
+++ .github/actions/test/run.sh
@@ -4,6 +4,7 @@ set -e
 # git config --global user.name "Andrea Ostuni"
 
 echo "$SSH_AUTH_SOCK"
+eval "$(ssh-agent)"
 ssh-add -L
 cat /etc/ssh_config
 ssh -T git@github.com
